c61124e69355dc2f094147214273748e238cf410
JonasHertzz <134273445+JonasHertzz@users.noreply.github.com>
2023-10-10 10:47:40 +0100

    10/10

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import { FaComputer } from 'react-icons/fa6'
+import imagemCabeçalho from './imagens/pexels-photo.jpeg'
+import './style.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <header>
+          <img src={imagemCabeçalho} alt='imagemCabeçalho' title='imagemCabeçalho'/>
+        </header>
+        <div>
+          <h1>
+            sobre
+          </h1>
+          <p>Me aperfeiçoei em Criação de Site, Lojas Virtual (E-commerce), Identidade Visual, Design Gráfico e Comunicação Visual. Minhas criações seguem sólidos conceitos do design, gestalt e teoria da cor sempre levando em consideração o tipo de negócio, concorrentes e melhor forma de impacto para disseminação da marca. Desenvolvo sites modernos, de fácil navegação e facilmente atualizáveis, com excelente visibilidade junto aos buscadores.
+          </p>
+          <h1>atuação</h1>
+          <FaComputer size={80} />
+          <p>Me aperfeiçoei em Criação de Site, Lojas Virtual (E-commerce), Identidade Visual, Design Gráfico e Comunicação Visual. Minhas criações seguem sólidos conceitos do design, gestalt e teoria da cor sempre levando em consideração o tipo de negócio, concorrentes e melhor forma de impacto para disseminação da marca. Desenvolvo sites modernos, de fácil navegação e facilmente atualizáveis, com excelente visibilidade junto aos buscadores.
+          </p>
+          <h2>Design Digital</h2>
+          <p>Me aperfeiçoei em Criação de Site, Lojas Virtual (E-commerce), Identidade Visual, Design Gráfico e Comunicação Visual. Minhas criações seguem sólidos conceitos do design, gestalt e teoria da cor sempre levando em consideração o tipo de negócio, concorrentes e melhor forma de impacto para disseminação da marca. Desenvolvo sites modernos, de fácil navegação e facilmente atualizáveis, com excelente visibilidade junto aos buscadores.
+          </p>
+          <h2>Design Gráfico</h2>
+          <p>Me aperfeiçoei em Criação de Site, Lojas Virtual (E-commerce), Identidade Visual, Design Gráfico e Comunicação Visual. Minhas criações seguem sólidos conceitos do design, gestalt e teoria da cor sempre levando em consideração o tipo de negócio, concorrentes e melhor forma de impacto para disseminação da marca. Desenvolvo sites modernos, de fácil navegação e facilmente atualizáveis, com excelente visibilidade junto aos buscadores.
+          </p>
+          <h2>identidade visual</h2>
+          <p>Me aperfeiçoei em Criação de Site, Lojas Virtual (E-commerce), Identidade Visual, Design Gráfico e Comunicação Visual. Minhas criações seguem sólidos conceitos do design, gestalt e teoria da cor sempre levando em consideração o tipo de negócio, concorrentes e melhor forma de impacto para disseminação da marca. Desenvolvo sites modernos, de fácil navegação e facilmente atualizáveis, com excelente visibilidade junto aos buscadores.
+          </p>
+          <h2>apresentações</h2>
+          <p>Me aperfeiçoei em Criação de Site, Lojas Virtual (E-commerce), Identidade Visual, Design Gráfico e Comunicação Visual. Minhas criações seguem sólidos conceitos do design, gestalt e teoria da cor sempre levando em consideração o tipo de negócio, concorrentes e melhor forma de impacto para disseminação da marca. Desenvolvo sites modernos, de fácil navegação e facilmente atualizáveis, com excelente visibilidade junto aos buscadores.
+          </p>
+          <h1>Portifólio</h1>
+        </div>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
